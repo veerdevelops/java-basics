0b5081d09b049ae8051699b6dab110e2fc8228aa
Veer <veergurnani20@gmail.com>
2026-06-22 12:53:02 +0530

    Renamed Loops to if_else and added else_if

--- src/if_else.java
+++ src/if_else.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Loops {
+public class if_else {
    public static void main(String[] args) {
     Scanner sc = new Scanner (System.in);
     System.out.println("enter your age : ");
