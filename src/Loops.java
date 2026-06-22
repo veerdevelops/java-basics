b697aef8a7ab125f1f85ba6157bff1f5191c3374
Veer <veergurnani20@gmail.com>
2026-06-22 12:37:49 +0530

    Added if-else age checker

---
+++ src/Loops.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Loops {
+   public static void main(String[] args) {
+    Scanner sc = new Scanner (System.in);
+    System.out.println("enter your age : ");
+    int age = sc.nextInt();
+    if(age>= 18) {
+        System.out.println("you are an adult.");
+    }else{
+        System.out.println("you are not an adult.");
+    }
+    sc.close();
+    }
+}
