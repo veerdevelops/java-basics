dca6c558abe2eef158347cd695d86c86f8f49e44
Veer <veergurnani20@gmail.com>
2026-06-24 15:20:59 +0530

    Added string function

---
+++ src/basicsofjava/string/func_string.java
@@ -0,0 +1,10 @@
+package basicsofjava.string;
+
+public class func_string {
+    public  static void main(String[] args) {
+        String original = "HELLO";
+        String result = original + "world";
+        System.out.println("Original: " + original);
+        System.out.println("returned: " + result);
+    }
+}
