a7e7621402bf4f8588bdf60580ca8eb692d65b67
Veer <veergurnani20@gmail.com>
2026-06-24 14:26:58 +0530

    added string length program

---
+++ src/basicsofjava/string/len_string.java
@@ -0,0 +1,8 @@
+package basicsofjava.string;
+
+public class len_string {
+   public  static void main(String[] args) {
+        String s = "HELLO WORLD";
+        System.out.println("length =" + s.length());
+    }
+}
